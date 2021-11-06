f8a378ec53e3bc5e794e72544fe09be58a0f360b
Derrick Bowles <derrick@smcreset.com>
2021-11-06 18:24:01 -0400

    Just writing some imports.  Nothing really built yet.

---
+++ parkdwn.py
@@ -0,0 +1,16 @@
+# This is the main code for the parkdwn app
+
+from PyQt6.QtWidgets import QApplication, QLabel, QCheckBox, QRadioButton, QComboBox, QPushButton, QTableWidget, QLineEdit, QSlider, QProgressBar, QWidget, QVBoxLayout, QTextEdit, QScrollBar, QMenuBar, QMenu, QMainWindow, QHBoxLayout
+
+app = QApplication([])
+label = QLabel()
+window = QWidget()
+menu_bar = QMenuBar()
+menu_item = QMenu()
+main_window = QMainWindow()
+vert_layout = QVBoxLayout()
+horiz_layout = QHBoxLayout()
+edit_space = QTextEdit()
+
+menu_bar.addWidget
+main_window.show()
